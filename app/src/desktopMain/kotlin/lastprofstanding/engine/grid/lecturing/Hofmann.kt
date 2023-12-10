075abe0dec6f3ec01265b4a9aca0e9e725ea3360
Knick21 <nick.buettner04@gmail.com>
2023-12-10 13:44:13 +0100

    feature 🍞 added spawn pattern

--- app/src/desktopMain/kotlin/lastprofstanding/engine/grid/lecturing/Hofmann.kt
+++ app/src/desktopMain/kotlin/lastprofstanding/engine/grid/lecturing/Hofmann.kt
@@ -1,14 +1,21 @@
 package lastprofstanding.engine.grid.lecturing
 
-import lastprofstanding.engine.grid.Grid
-import lastprofstanding.engine.grid.GridPosition
-import lastprofstanding.engine.grid.SpawnPattern
-import lastprofstanding.engine.grid.Weakness
+import lastprofstanding.engine.grid.*
 
 class Hofmann : Lecturer("Hofmann", 0.1f, Weakness(StroetmannMinion::class, 2, 2), null) {
     override val textRepresentation = "H"
 
     override fun getSpawnPattern(grid: Grid, position: GridPosition): SpawnPattern? {
-        TODO("Not yet implemented")
+        return createSpawnPattern(
+            position,
+            Pair(GridPosition(-2, -2), HofmannMinion()),
+            Pair(GridPosition(0, -2), HofmannMinion()),
+            Pair(GridPosition(2, -2), HofmannMinion()),
+            Pair(GridPosition(-2, 0), HofmannMinion()),
+            Pair(GridPosition(2, 0), HofmannMinion()),
+            Pair(GridPosition(-2, 2), HofmannMinion()),
+            Pair(GridPosition(0, 2), HofmannMinion()),
+            Pair(GridPosition(2, 2), HofmannMinion()),
+        )
     }
 }
