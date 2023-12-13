2712f95e0b1fd6d549c3145efde0ef06ad78e951
Zetsuboushii <zetsuboushii@gmail.com>
2023-12-13 01:55:12 +0100

    fix 🦆 import fix

--- app/src/desktopMain/kotlin/EndView.kt
+++ app/src/desktopMain/kotlin/EndView.kt
@@ -1,21 +1,20 @@
+
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.unit.sp
-import lastprofstanding.engine.StatsCounter
-import java.time.Duration
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.BitmapPainter
 import androidx.compose.ui.res.loadImageBitmap
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import lastprofstanding.engine.EngineState
 import lastprofstanding.engine.grid.lecturing.Lecturer
 import java.time.Duration
